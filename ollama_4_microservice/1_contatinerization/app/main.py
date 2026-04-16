5fbcbdf37c8abb2a5d285625fa4bdc429646235d
daniel77 <ygz0890421@gmail.com>
2026-04-16 12:03:19 +0800

    add : ollama_4_microservice structure builded.

---
+++ ollama_4_microservice/1_contatinerization/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+# 這是 Ollama 在 Docker 環境中存取宿主機的特殊位址
+OLLAMA_URL = "http://host.docker.internal:11434/api/generate"
+
+@app.get("/")
+async def root():
+    return {"message": "AI Microservice is running"}
+
+@app.post("/ask")
+async def ask_ai(prompt: str):
+    payload = {
+        "model": "qwen3:4b-instruct", # 確保你本機有先 pull 這個模型
+        "prompt": prompt,
+        "stream": False
+    }
+    
+    async with httpx.AsyncClient() as client: #防止塞車，可進出
+        # 設定較長的 timeout，因為 AI 推論需要時間
+        response = await client.post(OLLAMA_URL, json=payload, timeout=60.0)
+        return response.json()
---
+++ ollama_4_microservice/README.md
@@ -0,0 +1,14 @@
+# 我的AI技能 - microservice 篇
+
+### > 基本架構:
+![alt text](image.png)
+
+### > 套件
+```python
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+```
+
+---
